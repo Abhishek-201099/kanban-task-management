399fabb9f003d1a41635153d2e0515b15fba9f6c
Abhishek Bhattacharjee <ahishek1250@gmail.com>
2023-12-22 18:17:25 +0530

    feat: Added responsive style for aside and navbar with minor ui bug fixes, implemented slice for task and added add and delete and edit reducer for tasks

--- src/Store.js
+++ src/Store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import boardReducer from "./features/Boards/boardSlice";
+import taskReducer from "./features/Tasks/taskSlice";
 
 const store = configureStore({
   reducer: {
     boards: boardReducer,
+    tasks: taskReducer,
   },
 });
 
---
+++ src/features/Tasks/taskSlice.js
@@ -0,0 +1,66 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialTaskState = {
+  tasksData: [
+    {
+      taskName: "Sample Task",
+      taskForBoard: "Sample Board",
+      taskForCol: "Todo",
+      taskDescription: "lorem ipsum woho",
+      subtasks: [
+        { subtask: "Design the task board", subtaskStatus: "unchecked" },
+        { subtask: "Clear the board once done", subtaskStatus: "checked" },
+      ],
+    },
+  ],
+};
+
+const taskSlice = createSlice({
+  name: "tasks",
+  initialState: initialTaskState,
+  reducers: {
+    addTask(state, action) {
+      state.tasksData = [...state.tasksData, action.payload.task];
+    },
+    editTask(state, action) {
+      const { updatedTask } = action.payload;
+      const taskToUpdate = state.tasksData.find(
+        (task) =>
+          task.taskName === updatedTask.prevTaskName &&
+          task.taskForBoard === updatedTask.taskForBoard
+      );
+
+      if (taskToUpdate) {
+        taskToUpdate.taskName = updatedTask.taskName;
+        taskToUpdate.taskForBoard = updatedTask.taskForBoard;
+        taskToUpdate.taskDescription = updatedTask.taskDescription;
+        taskToUpdate.taskForCol = updatedTask.taskForCol;
+        taskToUpdate.subtasks = updatedTask.subtasks;
+      }
+    },
+    deleteTask(state, action) {
+      const { taskToRemove } = action.payload;
+      const taskToDelete = state.tasksData.find(
+        (task) =>
+          task.taskName === taskToRemove.taskName &&
+          task.taskForBoard === taskToRemove.taskForBoard
+      );
+
+      if (taskToDelete) {
+        state.tasksData = state.tasksData.filter(
+          (task) =>
+            task.taskName !== taskToDelete.taskName &&
+            task.taskForBoard === taskToDelete.taskForBoard
+        );
+      }
+    },
+  },
+});
+
+export const { addTask } = taskSlice.actions;
+
+export default taskSlice.reducer;
+
+function getTasksData(state) {
+  return state.tasks.tasksData;
+}
